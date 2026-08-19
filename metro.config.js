ccb860f5c3ebc27ea611e803fc515c621e7082c1
Rizwan Ullah <rizwangul2426@gmail.com>
2026-08-18 21:48:24 -0700

    Add wasm assetExts in metro.config.js and set single web output

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Enable .wasm resolution for expo-sqlite
+config.resolver.assetExts.push("wasm");
+
 module.exports = withNativeWind(config, { input: "./global.css" });
